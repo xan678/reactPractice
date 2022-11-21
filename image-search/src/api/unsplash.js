fd4ad3edb68b3cda6eebca5c38ce20992518bccb
Ankush Ojha <alpha98689@gmail.com>
2022-11-21 18:59:19 +0530

    changes

--- image-search/src/api/unsplash.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-//Create instance of axios client 
-export default axios.create({
-    baseURL : "https://api.unsplash.com/",
-    headers : {
-        Authorization: "Client-ID kTb11oLTJTLpVSIA40q5SKN_fWWdHs0ziFwgcuIlVVQ"
-    }
-});
---
+++ songs-app/src/reducers/index.js
@@ -0,0 +1,13 @@
+const songsReducer = () => {
+    return [
+        {title: 'No Scrubs', duration: '4:05'},
+        {title: 'Macarana', duration: '2:35'},
+        {title: 'All Star', duration: '3:15'},
+        {title: 'I want it that way', duration: '1:25'},
+    ];
+};
+
+
+const selectedSongReducer = (selectedSong = null,action) => {
+
+}
