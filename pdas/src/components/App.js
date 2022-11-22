ce8f949a6d36c40e6b268bae9951c764402044b2
Ankush Ojha <alpha98689@gmail.com>
2022-11-22 14:52:50 +0530

    new stuff

--- pdas/src/components/App.js
+++ pdas/src/components/App.js
@@ -7,11 +7,45 @@ import SiriImage from "../images/siri.png"
 function App (){
     return (
         <div>
-            <div> Personal Digital Assitants</div>
+            <section className = "hero is-primary">
+                <div className = "hero-body">
+                    <p className = "title">
+                        Personal Digital Assitants
+                    </p>
+                </div>
+            </section>
 
-            <ProfileCard title = "Alexa" handle = "@alexa99" image = {AlexaImage}/>
-            <ProfileCard title = "Cortana" handle = "@cortana" image = {CortanaImage}/>
-            <ProfileCard title = "Siri" handle = "@siri" image = {SiriImage}/>
+            <div className = "container">
+                <section className = "section">
+                    <div className = "columns">
+                        <div className = "column is-2">
+                            <ProfileCard 
+                                title = "Alexa" 
+                                handle = "@alexa99" 
+                                image = {AlexaImage}
+                                description = "Alexa was created by Amazon to help you buy things" 
+                            />
+                        </div>
+                        <div className = "column is-2">
+                            <ProfileCard 
+                                title = "Cortana" 
+                                handle = "@cortana" 
+                                image = {CortanaImage}
+                                description = "Cortana was made by Microsoft. Who know what it does?"
+                            />
+                        </div>
+                        <div className = "column is-2">
+                            <ProfileCard 
+                                title = "Siri" 
+                                handle = "@siri" 
+                                image = {SiriImage}
+                                description = "Siri was made by Apple and is being phased out"
+                            />
+                        </div>
+                    </div>
+                </section>
+            </div>
+
         </div>
     )
 }
